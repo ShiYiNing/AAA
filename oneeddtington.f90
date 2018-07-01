1c39b802e480abcb1311665fc2e48bb2054c8778
578460165syn <40736488+578460165syn@users.noreply.github.com>
2018-07-01 21:31:17 +0800

    Update oneeddtington.f90

--- oneeddtington.f90
+++ oneeddtington.f90
@@ -2,42 +2,27 @@ program eddington
 IMPLICIT REAL(4)(A-H, K-Z)
 integer::i,n,l1,l,j
 parameter(n=2)
-real(4):: fdu1(n),fuu1(n),fdu1w(n),fuu1w(n),fdu1g(n),fuu1g(n),fdu1wg(n),fuu1wg(n),fdu(n),fuu(n),Bench(1000),mu(90),p(257)
-!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-!Do 2000   uii = -1.00 , 2.01 ,0.1    !31
-!t0=10.0**uii
-!Do 1000 ujj=0.0099,1.01,0.05   !21
-!u0 = min(ujj,1.0)
-!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc 
-!Do 2000 ujj = 1 , 100
+real(4):: fdu1(n),fuu1(n),fdu1w(n),fuu1w(n),fdu1g(n),fuu1g(n),fdu1wg(n),fuu1wg(n),fdu(n),fuu(n),p(257)
 u0 = 0.5
 u = 0.7074532
-!u0=0.5
-!Do 3000 uii = 1 , 20
-!Do 2000 ujj = 1 , 100
-!t0=ujj
-!t0=84.372735353535400
-t0=66.2377535353535
+Del_varph = 0.0
+t0 = 66.2377535353535
 !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-sigmaw=0.000 ; sigmag=0.000
-!sigmaw=-8.224879532622400e-06 ; sigmag=4.351226591331250e-05
-!sigmaw=-9.982859170768868e-06 ; sigmag=5.281254528887848e-05                                                                                 
+sigmaw=0.000 ; sigmag=0.000                                                                                
 ww=1-0.005532247346190 ; g=0.780812663379802 ; f0=1 ; rr=0.0
-!!ww=1-0.004005156074249 ; g=0.772733857941188 ; f0=1 ; rr=0.0
 pi=3.1415927
 fdu1(1)=0 ; fdu1(2)=0 ; fuu1(1)=0 ; fuu1(2)=0
 fdu1w(1)=0 ; fdu1w(2)=0 ; fuu1w(1)=0 ; fuu1w(2)=0
 fdu1g(1)=0 ; fdu1g(2)=0 ; fuu1g(1)=0 ; fuu1g(2)=0
 fdu(1)=0 ; fdu(2)=0 ; fuu(1)=0 ; fuu(2)=0
-!ff=g**2
 ff=0
 ww1=ww
 g1=(g-ff)/(1-ff)
 sigmag1=sigmag/(1.-ff)
-!cccccccccccccccccccccccccccc直射项的处理cccccccccccccccccccccccccccccccccccccccccccccccccc
+!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 n1=1-ww1*ff
 mm1=exp(-n1*t0/u0)
-!cccccccccccccccccccccccccccc均匀层结Eddtington解cccccccccccccccccccccccccccccccccccccccccc
+!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 y1=1.75-(0.75*ff+1)*ww1-0.75*(1-ff)*ww1*g1
 y2=-0.25-(0.75*ff-1)*ww1-0.75*(1-ff)*ww1*g1
 y_u_5=(1-ff)*ww1*(0.5-0.75*g1*u0)*pi*f0 ; y_d_5=(1-ff)*ww1*(0.5+0.75*g1*u0)*pi*f0
@@ -50,7 +35,7 @@ C_10=(alpha_u*mm1*(rr*G_d_1-G_u_1+rr*u0*pi*f0)+(alpha_d-rr*alpha_u)*G_d_1*mm2)/(
 C_20=(-G_d_1-alpha_d*C_10*mm2)/alpha_u
 fuu1(1)=alpha_u*C_10*mm2+alpha_d*C_20+G_u_1
 fdu1(2)=alpha_d*C_10+alpha_u*C_20*mm2+G_d_1*mm1
-!ccccccccccccccccccccccccccccsigmaw方程解cccccccccccccccccccccccccccccccccccccc
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 if ( sigmaw .ne. 0. ) then
 y_1w=-(0.75*ff+1)-0.75*(1-ff)*g1 ; y_2w=-(0.75*ff-1)-0.75*(1-ff)*g1
 y_u_5w=(1-ff)*(0.5-0.75*g1*u0)*pi*f0 ; y_d_5w=(1-ff)*(0.5+0.75*g1*u0)*pi*f0
@@ -119,7 +104,7 @@ fuu1w(1)=D_u_1w*mm2+D_u_2w+eta_u_5w
 fdu1w(2)=D_d_1w+D_d_2w*mm2+eta_d_1w*t0+eta_d_2w*t0*mm2+eta_d_3w*t0*t0&
 &        +eta_d_4w*t0*t0*mm2+eta_d_5w*mm1+eta_d_6w*t0*mm1+eta_d_7w*t0*t0*mm1
 endif
-!ccccccccccccccccccccccccccccsigmag方程解cccccccccccccccccccccccccccccccccccccc
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 if ( sigmag .ne. 0 ) then
 y_1g=-0.75*(1-ff)*ww1 ; y_2g=y_1g
 y_u_5g=-0.75*(1-ff)*u0*pi*f0*ww1 ; y_d_5g=-y_u_5g
@@ -190,27 +175,6 @@ fdu(2)=fdu(2)+u0*pi*f0*mm1
 TI0=fuu(1)/(2*pi*pi)
 TI1=fuu(1)/(4*pi*pi)
 
-!open(12,file="mu256.txt")
-!open(111,file="I_u.txt")
-!do 1001 i=1,90
-!read(12,*)  mu(i)
-!zmu=mu(i)
-!PP1=zmu
-!PP2=0.5*(3*zmu**2-1)
-!PP3=0.5*(5*zmu**3-3*zmu)
-!AP1=-6.210782379474913*u0+5.783227630015377  ; BP1=-2.761103001625419*u0+0.993681514943675
-!AP2=-3.054554127360934*u0+4.474765546640607  ; BP2=2.143685150869431*u0-0.129630387218231
-!AP3=0.019880783686420*u0+0.159300336219902   ; BP3=-0.589317041597288+0.985180986451162
-!AP4=-5.320711000050651*u0+14.226748298111872 ; BP4=0.636797921641261*u0-0.211448936708153
-!AP5=-0.300290008968647*u0+1.042440506457924  ; BP5=-0.080448737918686*u0+0.190493965417525
-!A2=AP1+AP2*exp(-AP3*t0)+AP4*exp(-AP5*t0)
-!B2=BP1+BP2*exp(-BP3*t0)+BP4*exp(-BP5*t0)
-!TI=TI0+(3*PP1-sqrt(3.)*(4./15)*A2*(PP2-B2*PP3)+sqrt(3.)*A2*(1-zmu)**4)*TI1
-!write(111,*) TI
-!1001 end do
-!close(12)
-
-
 PP1=u
 PP2=0.5*(3*u**2-1)
 PP3=0.5*(5*u**3-3*u)
@@ -223,9 +187,7 @@ A2=AP1+AP2*exp(-AP3*t0)+AP4*exp(-AP5*t0)
 B2=BP1+BP2*exp(-BP3*t0)+BP4*exp(-BP5*t0)
 TI=TI0+(3*PP1-sqrt(3.)*(4./15)*A2*(PP2-B2*PP3)+sqrt(3.)*A2*(1-u)**4)*TI1
 mm3=exp(-t0/u)
-open(100,file="I_phi.txt")
-Do Del_varph = 0.0, pi*1.00001, pi/180.0
-!cccccccccccccccccccccccccccc方位角平均单次散射近似cccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 TI_Rid_ref_a=u0*f0*ww1/(4*pi*u+4*pi*u0)*(1-3*g1*u*u0)*(1-mm3*mm1)
 mm4=u*u0/(u+u0)
 mm5=-mm4*(t0*mm1*mm3-mm4*(1-mm1*mm3))-t0/2*mm4*(1-mm1*mm3)
@@ -234,7 +196,7 @@ TI_Rid_ref_b=f0/(4*pi*u)*(1-3*g1*u*u0)*sigmaw*mm5
 TI_Rid_ref_c=3*ww1*f0/(4*pi*u)*u*(-u0)*sigmag*mm5
 end if
 TI_Rid_ref=TI_Rid_ref_a+TI_Rid_ref_b+TI_Rid_ref_c
-!cccccccccccccccccccccccccccc方位角单次散射近似cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
 A_n=0.0
 B_n=0.0
 Co_ta =  -1 * u * u0 + (1-u ** 2 ) ** 0.5 * (1-u0 ** 2) ** 0.5 * cos (Del_varph)
@@ -262,23 +224,5 @@ TI_AZI=TI_Rid_ref_azi-TI_Rid_ref+TI&
 &      +B1*(1-u)*(1-u0)*TI*cos(2*Del_varph)&
 &	   +C1*(1-u)*(1-u0)*TI*cos(3*Del_varph)&
 &	   +D1*(1-u)*(1-u0)*TI*cos(4*Del_varph)
-write( 100 , * ) TI_AZI
-end do
 !cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-
-!open(6,file='eddtingtonre.txt')
-!open(8,file='eddtingtontr.txt')
-!open(10,file='eddtingtonab.txt')
-!write(6,*) fuu(1)/fdu(1)
-!write(8,*) fdu(2)/fdu(1)
-!write(10,*) 1-(fuu(1)/fdu(1))-(fdu(2)/fdu(1))
-
-!open(16,file='re_homogeneous.txt')
-!open(18,file='tr_homogeneous.txt')
-!open(20,file='ab_homogeneous.txt')
-!write(16,*) fuu1(1)/(fdu1(1)+u0*pi*f0)
-!write(18,*) (fdu1(2)+u0*pi*f0*mm1)/(fdu1(1)+u0*pi*f0)
-!write(20,*) 1-fuu1(1)/(fdu1(1)+u0*pi*f0)-(fdu1(2)+u0*pi*f0*mm1)/(fdu1(1)+u0*pi*f0)
-!2000 enddo
-!3000 enddo
 end
